1936f3359ac121dc966c3d629c2637b0b8dd1e1e
Akash Singh <caelumpirata@gmail.com>
2023-02-02 13:04:01 +0530

    updated hello.cpp

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    cout << "hello there" << endl;
     return 0;
 }
